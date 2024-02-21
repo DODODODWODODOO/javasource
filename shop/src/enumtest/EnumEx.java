ddbe649e07202eea81ede4d27111d8cb4338a214
DongWan <dlehddhks125@naver.com>
2024-02-21 17:57:26 +0900

    2024-02-21

--- shop/src/enumtest/EnumEx.java
+++ shop/src/enumtest/EnumEx.java
@@ -8,44 +8,44 @@ public class EnumEx {
   public static void main(String[] args) {
     Week today = null;
 
-    // 오늘 날짜 가져오기
+    //오늘날짜 가져오기
     LocalDate now = LocalDate.now();
     // System.out.println(now); // 2024-02-14
 
-    // 오늘 날짜에 맞는 숫자를 리턴(1(월요일) ~ 7(일요일))
+    //오늘 날짜에 맞는 숫자 리턴(1(월요일) ~ 7(일요일))
     DayOfWeek week = now.getDayOfWeek();
 
     switch (week.getValue()) {
       case 1:
-        today = week.MONDAY;
+        today = Week.MONDAY;
         break;
       case 2:
-        today = week.TUESDAY;
+        today = Week.TUESDAY;
         break;
       case 3:
-        today = week.WEDNESDAY;
+        today = Week.WEDNESDAY;
         break;
       case 4:
-        today = week.THURSDAY;
+        today = Week.TUESDAY;
         break;
       case 5:
-        today = week.FRIDAY;
+        today = Week.FRIDAY;
         break;
       case 6:
-        today = week.SATURDAY;
+        today = Week.SATURDAY;
         break;
       case 7:
-        today = week.SUNDAY;
+        today = Week.SUNDAY;
         break;
       default:
         break;
     }
 
     System.out.println("오늘 요일 : " + today);
-    if (today == week.SUNDAY) {
-      System.out.println("휴식을 취합니다");
+    if (today == Week.SUNDAY) {
+      System.out.println("휴식을 취합니다.");
     } else {
-      System.out.println("열심히 공부한다");
+      System.out.println("열심히 자바 공부를 합니다.");
     }
   }
 }
